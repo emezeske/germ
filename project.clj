a83089733c448135c38752fa1fded80f5b45a9c3
Evan Mezeske <evan@meebo-inc.com>
2012-09-08 12:20:28 -0700

    Tidy up the project.clj file.

--- project.clj
+++ project.clj
@@ -1,13 +1,9 @@
 (defproject germs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Just playing around with physics"
+  :url "https://github.com/emezeske/germ"
   :license
     {:name "Eclipse Public License"
      :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins
-    [[lein-tarsier "0.9.4-SNAPSHOT"]]
-  :vimclojure-opts
-    {:repl true}
   :dependencies
     [[org.clojure/clojure "1.5.0-alpha4"]
      [quil "1.6.0"]])
